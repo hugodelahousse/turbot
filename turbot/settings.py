34e21e753947c81b078e0f497a20d56f5287f705
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-05-25 11:14:26 +0200

    Reddit: Put extensions in settings

--- turbot/settings.py
+++ turbot/settings.py
@@ -169,3 +169,5 @@ NIGHT_START = parser.parse(env("NIGHT_START", default="23:00")).time()
 NIGHT_END = parser.parse(env("NIGHT_END", default="09:00")).time()
 
 PHOTO_FSTRING = env("PHOTO_FSTRING", default="https://picsum.photos/200")
+
+REDDIT_VALID_EXTENSION = ["jpg", "png", "gif"]
