ceffe9ef70165d74ea33aa8e5e0779b0901dc5fd
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-10-16 21:15:24 +0200

    Admin: Improve poll admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Poll, Choice
 
-admin.site.register(Poll)
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_filter = ("channel", "anonymous")
+
+
 admin.site.register(Choice)
